3eaee1668fed386636e443428f2c76fcefde89fc
tylerlogsdon1 <30585221+tylerlogsdon1@users.noreply.github.com>
2017-08-05 23:54:42 -0500

    Add files via upload

--- Backup_Data .sh
+++ Backup_Data .sh
@@ -1,17 +1,13 @@
+#!/bin/bash
 xdotool windowminimize $(xdotool getactivewindow)
 notify-send "Backup Tool" "Backup Tool is running. You may keep working and you will be notified upon completion."
 rm BACKUPS.zip
-rm ./cache/DEBs/*.*
-rm ./cache/Documents/*.*
-rm ./cache/Downloads/*.*
-rm ./cache/Media/*.*
-rm ./cache/Music/*.*
-cp /var/cache/apt/archives/*.deb ./cache/DEBs/
-cp ~/Documents/*.* ./cache/Documents/
-cp ~/Pictures/*.* ./cache/Media/
-cp ~/Videos/*.* ./cache/Media/
-cp ~/Downloads/*.* ./cache/Downloads/
-cp ~/Music/*.* ./cache/Music/
+rsync -a /var/cache/apt/archives/*.deb ./cache/DEBs/
+rsync -a ~/Documents/*.* ./cache/Documents/
+rsync -a ~/Pictures/*.* ./cache/Media/
+rsync -a ~/Videos/*.* ./cache/Media/
+rsync -a ~/Downloads/*.* ./cache/Downloads/
+rsync -a ~/Music/*.* ./cache/Music/
 cd ./cache
 zip -r BACKUPS.zip .
 mv BACKUPS.zip ..
@@ -21,6 +17,4 @@ rm ./cache/Documents/*.*
 rm ./cache/Downloads/*.*
 rm ./cache/Media/*.*
 rm ./cache/Music/*.*
-notify-send "Backup Tool" "Backup Tool has been packaged inside of BACKUPS.zip to make it portable."
 notify-send "Backup Tool" "Backup Tool has completed. Find your files in BACKUPS.zip."
-
